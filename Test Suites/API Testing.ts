1dd4047a372e19c139d356cbcb5f8a3093cf46fc
adria <adria@DESKTOP-2B5N6V0>
2023-06-01 18:54:07 +0700

    Making Test Suite

---
+++ Test Suites/API Testing.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>API Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8f020721-be47-4813-966f-aaa1b55d1ac2</testSuiteGuid>
+</TestSuiteEntity>
